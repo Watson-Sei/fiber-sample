0320ccd30ecdfbeb5be763bacb3718ff234dd498
Watson-Sei <seinabehack@gmail.com>
2021-01-19 15:20:31 +0900

    add websocket chat

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/Watson-Sei/fiber-sample
 go 1.15
 
 require (
+	github.com/andybalholm/brotli v1.0.1 // indirect
 	github.com/gofiber/fiber/v2 v2.3.3 // indirect
 	github.com/gofiber/template v1.6.6 // indirect
+	github.com/gofiber/websocket/v2 v2.0.2 // indirect
+	github.com/klauspost/compress v1.11.7 // indirect
+	github.com/savsgio/gotils v0.0.0-20210105085219-0567298fdcac // indirect
+	github.com/valyala/fasthttp v1.19.0 // indirect
+	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78 // indirect
 )
--- server.go
+++ server.go
@@ -2,21 +2,85 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
+	"log"
 )
 
+type client struct{} // 必要に応じて、このタイプにデータを追加します。
+
+var clients = make(map[*websocket.Conn]client)
+var register = make(chan *websocket.Conn)
+var broadcast = make(chan string)
+var unregister = make(chan *websocket.Conn)
+
+func runHub()  {
+	for {
+		select {
+		case connection := <-register:
+			clients[connection] = client{}
+			log.Println("connection registered")
+
+		case message := <-broadcast:
+			log.Println("message received:", message)
+
+			// Send the message to all client
+			for connection := range clients {
+				if err := connection.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
+					log.Println("write error: ", err)
+
+					unregister <- connection
+					connection.WriteMessage(websocket.CloseMessage, []byte{})
+					connection.Close()
+				}
+			}
+		case connection := <-unregister:
+			delete(clients, connection)
+			log.Println("connection unregistered")
+		}
+	}
+}
+
 func main()  {
-	engine := html.New("./templates", ".html")
+	app := fiber.New()
 
-	app := fiber.New(fiber.Config{
-		Views: engine,
+	app.Static("/", "./templates/index.html")
+
+	app.Use(func(c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			return c.Next()
+		}
+		return c.SendStatus(fiber.StatusUpgradeRequired)
 	})
 
-	app.Get("/", func(ctx *fiber.Ctx) error {
-		return ctx.Render("index", fiber.Map{
-			"title": "Hello, World",
-		})
-	})
+	go runHub()
+
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		// When the function returns, unregister the client and close the connection
+		defer func() {
+			unregister <- c
+			c.Close()
+		}()
+
+		// Register the client
+		register <- c
+
+		for {
+			messageType, message, err := c.ReadMessage()
+			if err != nil {
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+					log.Println("read error: ", err)
+				}
+				return // Calls the deferred function, i.e. closes the connection on error
+			}
+
+			if messageType == websocket.TextMessage {
+				// Broadcast the received message
+				broadcast <- string(message)
+			} else {
+				log.Println("websocket message received of type", messageType)
+			}
+		}
+	}))
 
 	app.Listen(":3000")
 }
